8fddb3d96d1eafe5248a42366ebd0fcf78b06708
rabyj <Joanny.Raby@usherbrooke.ca>
2022-04-12 17:01:05 -0400

    First commit

---
+++ link_dotfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -sf ~/dotfiles/ssh-config ~/.ssh/config
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.comet.config ~/.comet.config
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/vscode/.pylintrc ~/.pylintrc
+ln -sf ~/dotfiles/terminator-config ~/.config/terminator/config
