c01749706a6c726fea48ab977544d291b76a96a6
rabyj <Joanny.Raby@usherbrooke.ca>
2022-11-15 13:32:58 -0500

    Better dotfiles linking script

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-ln -sf ~/dotfiles/ssh-config ~/.ssh/config
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.comet.config ~/.comet.config
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/vscode/.pylintrc ~/.pylintrc
-ln -sf ~/dotfiles/terminator-config ~/.config/terminator/config
+echo "Use 'local' or 'narval'."
+if [ "$1" = "local" ];
+then
+  ln -svf ~/dotfiles/ssh-config ~/.ssh/config
+  ln -svf ~/dotfiles/.bashrc ~/.bashrc
+  ln -svf ~/dotfiles/.comet.config ~/.comet.config
+  ln -svf ~/dotfiles/.gitconfig ~/.gitconfig
+  ln -svf ~/dotfiles/terminator-config ~/.config/terminator/config
+
+  ln -svf ~/dotfiles/vscode/.pylintrc ~/.pylintrc
+  ln -svf ~/dotfiles/vscode/general-settings.json /home/local/USHERBROOKE/rabj2301/.config/Code/User/settings.json
+  ln -svf ~/dotfiles/vscode/workspaces/EPILAP.code-workspace /home/local/USHERBROOKE/rabj2301/Projects/epilap/EPILAP.code-workspace
+fi
+
+if [ "$1" = "narval" ];
+then
+  folder="$HOME/project-rabyj/sources/dotfiles/vscode/workspaces"
+  ln -svf "${folder}/NARVAL.code-workspace" /home/rabyj/project-rabyj/NARVAL.code-workspace
+  ln -svf "${folder}/narval-server-settings.json" /home/rabyj/.vscode-server/data/Machine/settings.json
+fi
