48aff6b808a2a13b9a5b7b77ad2c7dd27f7c8cf2
rabyj <Joanny.Raby@usherbrooke.ca>
2023-09-29 14:41:55 -0400

    Add home to link_dotfiles

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2086
-echo "Use 'local' or 'narval'."
+echo "Use one of the following arguments: [local, narval, home]"
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 
 if [ "$1" = "local" ];
@@ -20,8 +20,24 @@ then
   ln -svf ${SCRIPT_DIR}/other_programs/doublecmd_shortcuts.scf ~/.config/doublecmd/shortcuts.scf
 
   ln -svf ${SCRIPT_DIR}/vscode/shellcheckrc ~/.shellcheckrc
-  ln -svf ${SCRIPT_DIR}/vscode/general-settings.json /home/local/USHERBROOKE/rabj2301/.config/Code/User/settings.json
-  ln -svf ${SCRIPT_DIR}/vscode/workspaces/EPILAP.code-workspace /home/local/USHERBROOKE/rabj2301/Projects/epilap/EPILAP.code-workspace
+  ln -svf ${SCRIPT_DIR}/vscode/general-settings.json ~/.config/Code/User/settings.json
+  ln -svf ${SCRIPT_DIR}/vscode/workspaces/EPILAP.code-workspace ~/Projects/epilap/EPILAP.code-workspace
+fi
+
+if [ "$1" = "home" ];
+then
+  ln -svf ${SCRIPT_DIR}/linux/ssh-config ~/.ssh/config
+  ln -svf ${SCRIPT_DIR}/linux/Xmodmap ~/.Xmodmap # for keyboard mapping, use in startup apps
+  ln -svf ${SCRIPT_DIR}/linux/bashrc ~/.bashrc
+  ln -svf ${SCRIPT_DIR}/linux/inputrc ~/.inputrc
+  ln -svf ${SCRIPT_DIR}/linux/mimeapps.list ~/.config/mimeapps.list
+  ln -svf ${SCRIPT_DIR}/git/gitconfig ~/.gitconfig
+  ln -svf ${SCRIPT_DIR}/git/attributes ~/.config/git/attributes
+  ln -svf ${SCRIPT_DIR}/other_programs/comet.config ~/.comet.config
+
+  ln -svf ${SCRIPT_DIR}/vscode/shellcheckrc ~/.shellcheckrc
+  # ln -svf ${SCRIPT_DIR}/vscode/general-settings.json ~/.config/Code/User/settings.json
+  # ln -svf ${SCRIPT_DIR}/vscode/workspaces/EPILAP.code-workspace ~/Projects/epilap/EPILAP.code-workspace
 fi
 
 if [ "$1" = "narval" ];
@@ -30,8 +46,8 @@ then
   ln -svf "${gen_folder}/linux/inputrc" ~/.inputrc
 
   vs_folder="${gen_folder}/vscode/workspaces"
-  ln -svf "${vs_folder}/NARVAL.code-workspace" /home/rabyj/project-rabyj/NARVAL.code-workspace
-  ln -svf "${vs_folder}/narval-server-settings.json" /home/rabyj/.vscode-server/data/Machine/settings.json
+  ln -svf "${vs_folder}/NARVAL.code-workspace" ~/project-rabyj/NARVAL.code-workspace
+  ln -svf "${vs_folder}/narval-server-settings.json" ~/.vscode-server/data/Machine/settings.json
   ln -svf ${gen_folder}/vscode/shellcheckrc ~/.shellcheckrc
 
   cluster_folder="${gen_folder}/clusters"
